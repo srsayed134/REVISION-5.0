d48faa70d5ea61a20015f2e768f8d4f21c2fa420
Sayedur Rahman <srsayed134@gmail.com>
2026-08-29 13:12:42 +0600

    Loop in python

---
+++ Python/lesson_08.py
@@ -0,0 +1,76 @@
+# Challenge 1
+"""
+for number in range(1,6):
+    print(number)
+"""
+
+#Challenge 2
+"""
+for number in range(20):
+    if number % 2 == 0:
+        print(number)
+"""
+
+#Challenge 3
+"""
+for numbe in range(10,0,-1):
+    print(numbe)
+"""
+
+#Challenge 4
+"""
+language = "Python"
+
+for letter in language:
+    if letter == "P":
+        print(letter)
+    elif letter == "t":
+        print(letter)
+    elif letter == "o":
+        print(letter)
+"""
+
+#Challenge 5
+"""
+number = 1
+
+while number <= 10:
+    print(number) 
+    number += 1
+"""
+
+#Challenge 6 
+"""
+number = 1
+while number <= 10:
+    print(number)
+    if number == 7:
+        break
+    number +=1
+"""
+
+#Challenge 7
+"""
+for number in range(1,11):
+    if number == 5:
+        continue
+    print(number)
+"""
+
+#Challenge 8
+"""
+for number in range(1,11):
+    if number % 2 == 0:
+        print(f"{number} is even number")
+    else:
+        print(f"{number} is odd number")
+"""
+
+#Challenge 9
+"""
+guess_number = int(input("Write a number: "))
+
+for number in range(0, 11):
+    result = guess_number * number
+    print(f"{guess_number} * {number} = {result}")
+"""
